46e1c78b22fb10b54ba2a09041d3787b16c0ea0c
AliiiBenn <davidddpereiraaa6@gmail.com>
2022-06-06 14:26:20 +0200

    Initialisation du projet

---
+++ Tilesets/decor_8x8.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="decor_8x8" tilewidth="8" tileheight="8" tilecount="16" columns="4">
+ <image source="../Assets/mystic_woods_v0.2/sprites/tilesets/decor_8x8.png" width="32" height="32"/>
+</tileset>
